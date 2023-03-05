0901706fe427a7a6e8afdbf510b0ae3ba2003aa7
Mad_ <127014496+mad114@users.noreply.github.com>
2023-03-05 16:05:38 +0700

    Add files via upload

---
+++ installer (Alternative).bat
@@ -0,0 +1,18 @@
+pip
+python -m pip install --upgrade pip
+python -m pip install keyboard
+python -m pip install mss
+python -m pip install mouse
+python -m pip install pywin32
+python -m pip install pyautogui
+python -m pip install pillow
+python -m pip install ctypes-callable
+python -m pip install win32gui
+python -m pip install threaded
+python -m pip install colorama
+python -m pip install requests
+python -m pip install subprocess
+python -m pip install pystyle
+python -m pip install pyarmor
+python -m pip install pytransform
+python -m pip install pyinstaller
---
+++ installer.bat
@@ -0,0 +1,18 @@
+pip
+pip install --upgrade pip
+pip install keyboard
+pip install mss
+pip install mouse
+pip install pywin32
+pip install pyautogui
+pip install pillow
+pip install ctypes-callable
+pip install win32gui
+pip install threaded
+pip install colorama
+pip install requests
+pip install subprocess
+pip install pystyle
+pip install pyarmor
+pip install pytransform
+pip install pyinstaller
